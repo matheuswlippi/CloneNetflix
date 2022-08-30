234b2b38faead7967aa2c3639392d62a9f61c51f
MatheusLippi <102246955+matheuswlippi@users.noreply.github.com>
2022-08-30 20:26:09 -0300

    Primeira Publicação

---
+++ ReactJs/clonenetflix/src/App.js
@@ -0,0 +1,34 @@
+import React,{ useState, useEffect } from 'react'
+import './App.css'
+import Tmdb from './Tmdb'
+import Header from './components/Header'
+
+
+export default() => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(()=> {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      console.log(list)
+      setMovieList(list)
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length-1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      console.log(chosenInfo)
+      setFeaturedData(chosenInfo)
+    }
+    loadAll()
+  }, [])
+
+  return (
+    <div className="page">
+      <Header black={blackHeader}/>
+    </div>
+  )
+}
+
+
